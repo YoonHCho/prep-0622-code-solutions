f67a3bb06b183e3b5900f25ee7968eab5ae2bcb8
Yoon Hwan Cho <yhcho0714@gmail.com>
2022-06-13 06:00:08 +0000

    still working on prep-javascript-arrays

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,8 @@
+const languages = ['html', 'css', 'javascript', 'reach', 'node', 'postgres'];
+console.log('languages array', languages);
+const firstElement = languages[0];
+console.log('The first element of the array is:', firstElement);
+const thirdElement = languages[2];
+console.log('The third element of the array is:', thirdElement);
+const length = languages.length;
+console.log('The length of the languages array is:', length);
