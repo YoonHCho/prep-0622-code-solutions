0f70401317dde36f703f0db9c7e772cfc9fc3e7a
Yoon Hwan Cho <yhcho0714@gmail.com>
2022-06-14 03:38:38 +0000

    Completed prep-javascript-arrays exercise

--- prep-javascript-arrays/main.js
+++ prep-javascript-arrays/main.js
@@ -6,3 +6,5 @@ const thirdElement = languages[2];
 console.log('The third element of the array is:', thirdElement);
 const length = languages.length;
 console.log('The length of the languages array is:', length);
+const lastElement = languages[languages.length - 1];
+console.log('The last element of the array is:', lastElement);
