17122af74e6c8ce4180188ac9e8d6a1f0fb735e4
Yoon Hwan Cho <yhcho0714@gmail.com>
2022-06-15 22:22:31 +0000

    Completed prep-javascript-for-loops exercise

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,12 @@
+// create your loops here.
+for (var a = 0; a < 10; a++) {
+  console.log(a);
+}
+
+for (var b = 0; b < 20; b += 2) {
+  console.log(b);
+}
+
+for (var c = 100; c > 0; c--) {
+  console.log('Time till explosion: ', c);
+}
